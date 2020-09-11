ca42c571c12ed1973f62d1cfbd0f223e7ae71d8f
Menguelito <70752568+Menguelito@users.noreply.github.com>
2020-09-11 23:28:55 +0200

    Tekenen gelijkbenige driehoekc4.1

--- src/h04/Tekenen.java
+++ src/h04/Tekenen.java
@@ -10,8 +10,9 @@ public class Tekenen extends Applet{
 
     public void paint(Graphics g) {
         setBackground(Color.blue);
-        g.setColor(Color.yellow);
-        g.drawRect(20, 20, 100, 50);
-        g.fillRect(20, 80, 100, 50);
+        g.setColor(Color.cyan);
+        g.drawPolygon(new int[] {15, 35, 60}, new int [] {100, 50, 100}, 3);
+
+        
     }
 }
