02f1836a7994b78b063e8169f1d50959ef63fa2f
Menguelito <70752568+Menguelito@users.noreply.github.com>
2020-09-18 15:03:22 +0200

    Opdracht 5.2 Commit en Push

--- src/h05/Staafdiagram.java
+++ src/h05/Staafdiagram.java
@@ -1,4 +1,46 @@
 package h05;
 
-public class Staafdiagram {
+import java.applet.Applet;
+import java.awt.*;
+
+public class Staafdiagram extends Applet {
+
+    int Jeroen;
+    int Hans;
+    int Valerie;
+    int Breedte;
+
+    public void init() {
+
+        Jeroen = 50;
+        Hans = 100;
+        Valerie = 150;
+        Breedte = 50;
+
+    }
+    
+    public void paint(Graphics g) {
+
+        g.setColor(Color.red);
+        g.fillRect(50,100,Breedte,Jeroen);
+
+        g.setColor(Color.blue);
+        g.fillRect(100,50,Breedte,Hans);
+
+        g.setColor(Color.cyan);
+        g.fillRect(150,0,Breedte,Valerie);
+
+        g.setColor(Color.BLACK);
+        g.drawString("Jeroen",150,170);
+
+        g.setColor(Color.black);
+        g.drawString("Hans",100,170);
+
+        g.setColor(Color.BLACK);
+        g.drawString("Valerie",50,170);
+
+
+
+
+    }
 }
