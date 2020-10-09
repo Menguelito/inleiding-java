9ce74d952be97e24106c7f358621cc9c97ef6c9d
Menguelito <70752568+Menguelito@users.noreply.github.com>
2020-10-09 14:25:26 +0200

    Hoofdstuk 13 praktijkopdrachten

---
+++ src/h13/Praktijk1.java
@@ -0,0 +1,29 @@
+package h13;
+import java.awt.*;
+import java.applet.*;
+
+public class Praktijk1 extends Applet{
+    public void init() {}
+
+    public void paint(Graphics g) {
+        tekenBoom(g);
+    }
+
+    void tekenBoom(Graphics g) {
+        int x, y;
+        int hoogteBoom;
+        int breedteBoom;
+        x = 50;
+        y = 100;
+        hoogteBoom= 150;
+        breedteBoom = 25;
+
+        Color bruin = new Color(0x744915);
+        Color groen = new Color(0x1E600C);
+
+        g.setColor(bruin);
+        g.fillRect(x,y,breedteBoom,hoogteBoom);
+        g.setColor(groen);
+        g.fillOval(15,50,x*2,y);
+    }
+}
